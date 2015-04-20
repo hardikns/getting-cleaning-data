1875f68a76d97aa7ca13831ded10b71abcf79463
Hardik Shah <hardikns@gmail.com>
2015-04-20 13:10:14 +0530

    Q4_5 updated

--- week4/q4_5.R
+++ week4/q4_5.R
@@ -1,3 +1,8 @@
 library(quantmod)
 amzn = getSymbols("AMZN",auto.assign=FALSE)
 sampleTimes = index(amzn) 
+sampleTimes
+df <- data.frame(sapply(sampleTimes, format, "%Y"), sapply(sampleTimes, format, "%a"))
+names(df) <- c("year", "dayofweek")
+print(table(df$year))
+print(table(df))
